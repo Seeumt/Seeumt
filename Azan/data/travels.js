e55b518fe24046a5d2a22a79ad9d23b3d4cc4341
REN XIANG <Seeumt@cumt.edu.cn>
2019-08-27 19:58:32 +0800

    志愿活动页面数据分离

--- Azan/data/travels.js
+++ Azan/data/travels.js
@@ -1,36 +1,36 @@
 jsond(
-    {
-        "passages":[
-            {
-                "head":"images/resource/gallery4.jpg",
-                "username":"Sharon郭晓琳",
-                "title":"一生中总要有个夏天给清迈，6天5夜，追一场五彩斑斓的夏",
-                "cover":"images/wKgE2l0YrmCAArmJAAo0quQXhQo585.jpg",
-            },
-            {
-                "head":"images/resource/gallery3.jpg",
-                "username":"-滕六",
-                "title":"季夏远行，走进风景 || 自驾意大利南部，我闻到紫色的味道",
-                "cover":"images/wKgE2l0q9KGAeu_UADH3G_tZiEQ147.jpg",
+{
+	'passages':[
+                {
+                    'head':'images/resource/gallery4.jpg',
+                    'username':'Sharon郭晓琳',
+                    'title':'一生中总要有个夏天给清迈，6天5夜，追一场五彩斑斓的夏',
+                    'cover':'images/wKgE2l0YrmCAArmJAAo0quQXhQo585.jpg',
+                },
+                {
+                    'head':'images/resource/gallery3.jpg',
+                    'username':'-滕六',
+                    'title':'季夏远行，走进风景 || 自驾意大利南部，我闻到紫色的味道',
+                    'cover':'images/wKgE2l0q9KGAeu_UADH3G_tZiEQ147.jpg',
 
-            },
-            {
-                "head":"images/wKgBpVXIK5yALRbXAANqYfjtY4c15.jpeg",
-                "username":"张大枪",
-                "title":"迎初夏温柔的风，自驾魅力皖南线",
-                "cover":"images/wKgE2l0kA_aAN-VyABm5YjennSg373.jpg",
+                },
+                {
+                    'head':'images/wKgBpVXIK5yALRbXAANqYfjtY4c15.jpeg',
+                    'username':'张大枪',
+                    'title':'迎初夏温柔的风，自驾魅力皖南线',
+                    'cover':'images/wKgE2l0kA_aAN-VyABm5YjennSg373.jpg',
 
-            },
-            {
-                "head":"images/wKgE2l0m4sOAffGoAACZ4Stv2Ng20.jpeg",
-                "username":"Adrian.Z",
-                "title":"日本的秘境岛屿，游客来这里只有一个目的——与海豚共舞",
-                "cover":"images/wKgE2l0UvwGARn2QAC5lzBJgTAs686.jpg",
+                },
+                {
+                    'head':'images/wKgE2l0m4sOAffGoAACZ4Stv2Ng20.jpeg',
+                    'username':'Adrian.Z',
+                    'title':'日本的秘境岛屿，游客来这里只有一个目的——与海豚共舞',
+                    'cover':'images/wKgE2l0UvwGARn2QAC5lzBJgTAs686.jpg',
 
-            }
+                }
 
-        ]
-    }
+		]
+}
 
 
 );
